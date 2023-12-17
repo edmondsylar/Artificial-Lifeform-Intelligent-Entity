8475122ffdd7f792abce181269eb0d9c987d036d
edmondsylar <edmondmusiitwa@gmail.com>
2023-12-17 18:08:35 +0300

    vision component compelete

--- vision/Tests/eyes.py
+++ vision/Tests/eyes.py
@@ -6,76 +6,65 @@ import random
 from datetime import datetime
 
 # images directory
-images_dir = os.path.join(os.getcwd(), 'images')
 
-# define video capture object
-video = cv2.VideoCapture(1)
 
-# last immage save time
+from datetime import datetime
+import os
+import cv2
+import time
+import random
 from datetime import datetime
 
-# Initialize last_image_save_time with the current time
-last_image_save_time = datetime.now()
-
-def is_time_difference_five_seconds(last_image_save_time):
-    current_time = datetime.now()
-    time_difference = current_time - last_image_save_time
-    return time_difference.total_seconds() >= 2
+# image directory
+images_dir = os.path.join(os.getcwd(), 'images')
 
 
-# function to genration random name for the image
-def random_name():
-    # get the current time
-    current_time = time.strftime("%Y%m%d-%H%M%S")
+class Vision:
+    def __init__(self, camera_port=0):
+        self.last_image_save_time = datetime.now()
+        self.current_time = datetime.now()
+        self.video = cv2.VideoCapture(camera_port)
 
-    # get a random number
-    random_number = random.randint(0, 100000)
+    def is_time_difference_five_seconds(self):
+        current_time = datetime.now()
+        time_difference = current_time - self.last_image_save_time
+        return time_difference.total_seconds() >= 5
 
-    # return the random name
-    return current_time + str(random_number)
+    def random_name(self):
+        current_time = time.strftime("%Y%m%d-%H%M%S")
+        random_number = random.randint(0, 100000)
+        return current_time + str(random_number)
 
+    def save_image(self, frame, passed_time):
+        image_name = ''
+        time_difference = passed_time - self.current_time
+        seconds = time_difference.total_seconds()
 
-# function save an image given a frame
-def save_image(frame):
-    # get the current time
-    current_time = time.strftime("%Y%m%d-%H%M%S")
+        if seconds >= 2:
+            print(seconds)
+            self.current_time = datetime.now()
+            image_name = self.random_name()
+            print('saving image... {image_name}')
+            os.system('clear')
 
-    image_name = random_name()
+        print(fr'continuing.... {datetime.now().strftime("%H:%M:%S")}, Passed Seconds: {seconds}')
+        cv2.imwrite(os.path.join(images_dir, image_name + '.jpg'), frame)
 
-    # save the image
-    cv2.imwrite(os.path.join(images_dir, image_name + '.jpg'), frame)
-    return 'complete'
+    def checker(self, frame):
+        if self.is_time_difference_five_seconds():
+            self.save_image(frame, datetime.now())
+            self.last_image_save_time = datetime.now()
 
+    def capture(self):
+        while True:
+            ret, frame = self.video.read()
+            self.save_image(frame, datetime.now())
+            print('running...')
+            cv2.imshow('ALFIE-Vision (preview)', frame)
 
-def checker(frame):
-    global last_image_save_time
-    if is_time_difference_five_seconds(last_image_save_time):
-        # save the image
-        save_image(frame)
-        last_image_save_time = datetime.now()
-    else:
-        pass
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-
-def capture():
-    
-    # start the video capture
-    while True:
-        ret, frame = video.read()
-
-        # run the image checker
-        checker(frame)
-
-        # print
-        print('running...')
-        
-        # display the frame
-        cv2.imshow('ALFIE-Vision (preview)', frame)
-
-
-        # press 'q' to quit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-capture()
-
+vision = Vision()
+while True:
+    vision.capture()
--- vision/Tests/vEngine.py
+++ vision/Tests/vEngine.py
@@ -1,131 +1,119 @@
+import os
+import time
 from pathlib import Path
 import google.generativeai as genai
-import os
-import json
-from dotenv import load_dotenv
-import time
 from datetime import datetime
 
-images = [] # list of images
-perceptions = [] # list of perceptions from the model
+genai.configure(api_key='')
 
-# load .env file
-load_dotenv()
+class VisionEngine:
+  def __init__(self):
+    self.generation_config = {
+      "temperature": 0.4,
+      "top_p": 1,
+      "top_k": 32,
+      "max_output_tokens": 4096,
+    }
 
-# get the key.
-gemini_key = os.getenv('google_gemini_api_key')
+    self.safety_settings = [
+      {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+      },
+      {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+      },
+      {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+      },
+      {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+      }
+    ]
 
-# gemini api key
-genai.configure(api_key="AIzaSyDP20o8c9aoNlns4w49TywukOhXRUNHnxE")
-
-# Set up the model
-generation_config = {
-  "temperature": 0.4,
-  "top_p": 1,
-  "top_k": 32,
-  "max_output_tokens": 4096,
-}
-
-safety_settings = [
-  {
-    "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  }
-]
-
-model = genai.GenerativeModel(
-    model_name="gemini-pro-vision",
-    generation_config=generation_config,
-    safety_settings=safety_settings
+    self.model = genai.GenerativeModel(
+      model_name="gemini-pro-vision",
+      generation_config=self.generation_config,
+      safety_settings=self.safety_settings
     )
 
-def delete_images(image_urls):
-  for image_url in image_urls:
-    if os.path.isfile(image_url):
-      os.remove(image_url)
-    else:
-      print(f"Error: {image_url} not a valid filename")
+    self.perceptions = []
+    self.images = []
+    self.ablsolute_path = Path(__file__).parent.absolute()
 
+  def delete_images(self, image_urls):
+    for image_url in image_urls:
+      if os.path.isfile(image_url):
+        os.remove(image_url)
+      else:
+        print(f"Error: {image_url} not a valid filename")
 
-# generate insights function 
-def generate_insights(images, prompt):
-    # Set up the model
-    # ... (same as before)
+  def generate_insights(self, images, prompt):
+    print(fr'received {len(images)} images and processing... \n\nPrevious Perceptions: {self.perceptions}')
 
-    # Validate that images are present
     for img in images:
-        if not Path(img).exists():
-            raise FileNotFoundError(f"Could not find image: {img}")
+      if not Path(img).exists():
+        raise FileNotFoundError(f"Could not find image: {img}")
 
-    # Read image data
     image_parts = [{"mime_type": "image/jpeg", "data": Path(img).read_bytes()} for img in images]
-
-    # Create prompt parts
     prompt_parts = [prompt] + image_parts
-
-    # Generate and return response
-    response = model.generate_content(prompt_parts)
+    response = self.model.generate_content(prompt_parts)
     return response.text
 
-# absolute path to this file
-ablsolute_path = Path(__file__).parent.absolute()
+  def insights_loop(self):
+    try:
+      last_5_perceptions = self.perceptions[-5:]
+    except:
+      last_5_perceptions = []
 
-def insights_loop():
-  while True:
-    print('running...')
-    time.sleep(5)
+    while True:
+      print('running...')
+      time.sleep(1.5)
 
-    # get the images
-    for image in os.listdir(os.path.join(ablsolute_path, 'images')):
-      images.append(os.path.join(ablsolute_path, 'images', image))
+      for image in os.listdir(os.path.join(self.ablsolute_path, 'images')):
+        self.images.append(os.path.join(self.ablsolute_path, 'images', image))
 
-    if len(images) > 5:
-      prompt = f"""
+      if len(self.images) > 4:
+        prompt = fr"""
         #Grounding Instruction.
-        You are the eyes of a system that is trying to understand the world. you receive images every 2 seconds and just respond with imporatnt insights about the image. for the sysystem to properly understand what it needs, you also receive your previous outputs to help you better understand the concept of motion.
+        You are the eyes of a system that is trying to understand the world. you receive images every few seconds and just respond with imporatnt insights about the image. for the sysystem to properly understand what it needs, you also receive your previous outputs to help you better understand the concept of motion.
 
-        #previous outputs
-        {perceptions}
+        #Previous Observations.
+        {last_5_perceptions}
 
-        # Note: These can be empty if the system has just started.
+        # Note: The Observatiosn can be empty if the system is just starting up.
 
         Current Time: {datetime.now().strftime("%H:%M:%S")}
         """
-      # Get the last 5 images in the list
-      last_5_images = images[-5:]
+        last_5_images = self.images[-3:]
+        
+        try:
+          modelinsights = self.generate_insights(last_5_images, prompt)
 
-      # Generate insights
-      modelinsights = generate_insights(last_5_images, prompt)
+          insights = {
+            'time': datetime.now().strftime("%H:%M:%S"),
+            'insights': modelinsights
+          }
 
-      # Print the insights
-      
+          self.perceptions.append(fr'observation: {insights}, Date_of_observation: {datetime.now().strftime("%H:%M:%S")}')
+          os.system('clear')
+          
+          print(insights)
 
-      insights = {
-        'time': datetime.now().strftime("%H:%M:%S"),
-        'insights': modelinsights
-      }
 
-      print(insights)    
-      # Delete the images
-      delete_images(last_5_images)
+          self.delete_images(last_5_images)
+          time.sleep(1.5)
+        except Exception as e:
+          print('Experiencing an error,', e, ' \n Retrying in 1.5 seconds...')
+          time.sleep(1.5)
+          continue
+      else:
+        print('waiting')
+        continue
 
-      time.sleep(5)
-    else:
-      print('My Eyes are still closed, will resume in 12.5 seconds....')
-      continue
-
-# run the insights loop as the main thread
 if __name__ == '__main__':
-  insights_loop()
+  vision_engine = VisionEngine()
+  vision_engine.insights_loop()
