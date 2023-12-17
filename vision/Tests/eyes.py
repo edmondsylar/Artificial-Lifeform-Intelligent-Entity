fae74c2f6008cfe41774de9451d773481b01177f
edmondsylar <edmondmusiitwa@gmail.com>
2023-12-17 15:45:51 +0300

    vision engine experiencing issues

--- vision/Tests/eyes.py
+++ vision/Tests/eyes.py
@@ -2,34 +2,26 @@ import cv2
 import time
 import os
 
-def capture_images(interval=1.5, folder="images"):
-    # Create the images directory if it doesn't exist
-    if not os.path.exists(folder):
-        os.makedirs(folder)
+# images directory
+images_dir = os.path.join(os.getcwd(), 'images')
 
-    # Open the default camera
-    cap = cv2.VideoCapture(0)
+# define video capture object
+video = cv2.VideoCapture(1)
 
-    # Initialize the image counter
-    i = 0
+def capture():
+    
+    # start the video capture
+    while True:
+        ret, frame = video.read()
+        print (ret, frame)
 
-    try:
-        while True:
-            # Capture frame-by-frame
-            ret, frame = cap.read()
+        # display the frame
+        cv2.imshow('ALFIE-Vision (preview)', frame)
 
-            # Save the resulting frame
-            cv2.imwrite(os.path.join(folder, f'image{i}.jpg'), frame)
 
-            # Increment the image counter
-            i += 1
+        # press 'q' to quit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-            # Wait for the specified interval
-            time.sleep(interval)
-    finally:
-        # When everything is done, release the capture
-        cap.release()
-        cv2.destroyAllWindows()
+capture()
 
-if __name__ == "__main__":
-    capture_images()
